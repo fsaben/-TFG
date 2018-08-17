36637e661d039715e1cf28f33bce7e4e49d6fd39
fsaben <->
2018-08-17 21:03:36 +0200

    Raspberry code with Bluetooth disconnection logic

---
+++ Raspberry-Code/receive_data_v1_1.py
@@ -0,0 +1,39 @@
+import bluetooth
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(3, GPIO.OUT)
+GPIO.output(3, 0)
+
+#Functions
+def Leds():
+    while True:
+        data = client.recv(1024).decode()
+        print("Revived: %s" % data)
+        if data == "0":
+            GPIO.output(3, GPIO.LOW)
+            print("LED off")
+        if data == "1":
+            GPIO.output(3, GPIO.HIGH)
+            print("LED on")
+        if data == "q":
+            print("LEds Exit")
+            break
+
+#Main Loop
+while True:
+    server = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    server.bind(("", 0))
+    server.listen(1)
+
+    try:
+        client, address = server.accept()
+        print("Accepted connection from %s" % str(address))
+        Leds()
+
+    except bluetooth.BluetoothError as e:
+        print("Bluetooth not connected: [%s]\n\n" % str(e))
+
+    client.close()
+    server.close()
